afeea13f9eec8b039a9790a4d5a02d8baeae24bb
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-03-03 12:25:08 +0100

    build: update config

--- pilgiftsgrodorna/vite.config.js
+++ pilgiftsgrodorna/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
- 
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
 export default defineConfig({
   base: "/fed24d-grafiska-verktyg-pilgiftsgrodorna/",
+  plugins: [vue()],
 });
