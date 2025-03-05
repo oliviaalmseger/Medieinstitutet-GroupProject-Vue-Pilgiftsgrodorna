1460b0817028d7bb216266448f251bd9cc1bd598
Moa Magnfors <moa@magnfors.se>
2025-03-05 11:00:55 +0100

    feat: add InfoArticle component
    - script
    - html

---
+++ src/components/InfoArticle.vue
@@ -0,0 +1,28 @@
+<script setup>
+import InfoCard from './InfoCard.vue';
+</script>
+
+<template>
+    <article>
+        <InfoCard
+            heading="Visste du att?"
+            content="Grodornas gift kommer från insekter som grodorna äter. Om man har en pilgiftsgroda i fångenskap och ger den annan mat så blir den mindre giftig."
+        />
+        <InfoCard
+            heading="Visste du att?"
+            content="Pilgiftsgrodorna är nyfikna av sig och gärna utforskar sina omgivningar."
+        />
+        <InfoCard
+            heading="Visste du att?"
+            content="Giftet har traditionellt använts av indianer i Sydamerika för att förgifta pilar vid jakt och det är också därför de fått namnet pilgiftsgrodor."
+        />
+        <InfoCard
+            heading="Smarta färger"
+            content="En del av pilgiftsgrodorna är inte alls särskilt giftiga men drar nytta av giftgrodornas rykte – med starka färger och mönster ser man farlig ut."
+        />
+        <InfoCard
+            heading="Peta inte på en sån!"
+            content="Giftet från en enda gyllene kokoigiftgroda kan döda 10 000 möss eller mellan 10 och 20 människor!"
+        />
+    </article>
+</template>
