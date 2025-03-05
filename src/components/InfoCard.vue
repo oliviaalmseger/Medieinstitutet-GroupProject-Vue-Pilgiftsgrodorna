07e5e75905055f2a8d4f8048c765e410317a3cdf
Moa Magnfors <moa@magnfors.se>
2025-03-05 10:59:01 +0100

    style: add InfoCard scoped styling

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -11,3 +11,20 @@ defineProps({
         <p>{{ content }}</p>
     </section>
 </template>
+
+<style scoped lang="scss">
+section {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: $card-background-color;
+    border-radius: 22px;
+    border: 3px solid $border-color;
+    padding: 2rem 1.9rem;
+
+    h3 {
+        margin-bottom: 1rem;
+    }
+}
+</style>
