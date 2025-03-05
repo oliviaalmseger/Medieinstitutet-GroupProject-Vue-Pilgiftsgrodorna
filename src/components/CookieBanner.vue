13d1896c9da4a6e70fcb431950b0e9a9447e66b8
Alice Swahn <aliceswahn@hotmail.com>
2025-03-05 19:48:49 +0100

    style: add style to cookiebanner

--- src/components/CookieBanner.vue
+++ src/components/CookieBanner.vue
@@ -17,23 +17,67 @@ function acceptCookies() {
 
 <template>
   <div v-if="showCookie" class="cookie-banner">
-    <!-- Ska inte vara en h1 här, men ingen rubriksnivå matchar "Acceptera cookie" texten. -->
-    <h1>Acceptera cookies</h1>
-    <div>
-        <button @click="handleClick">Acceptera</button>
-    <!-- Vad ska hända när man trycker på neka knappen? -->
-        <button>Neka</button>
+    <!-- Ingen rubriksnivå matchar "Acceptera cookie" texten. I designen ska det vara h2, men det matchar inte den grafiska profilen, har improviserat lite-->
+    <p>Acceptera cookies</p>
+    <div class="cookie-button-container">
+        <div class="cookie-button" @click="handleClick">Acceptera</div>
+        <div class="cookie-button" @click="handleClick">Neka</div>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped lang="scss">
 .cookie-banner {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    background-color: #ff2700; 
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: $detail-color; 
+  padding: 40px 16px 32px 16px; 
+  
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+}
+
+p {
+  font-size: $h2-fs-mobile;
+  font-family: 'Luckiest Guy', sans-serif;
+  color:#000 ;
+}
+
+.cookie-button-container {
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+}
+
+.cookie-button {
+  background-image: url('/src/assets/figma_components/wood-sign-button.png');
+  background-size: cover;
+  background-position: center; 
+  border: none;
+  width: 117px;
+  height: 57px;
+  padding: 0; 
+  
+  display: flex; 
+  align-items: center; 
+  justify-content: center;
+  
+  cursor: pointer;
+  text-align: center;
+
+  font-size: $h2-fs-mobile;
+  font-family: 'Luckiest Guy', sans-serif;
+  color: #000;
+}
+
+@media (min-width: 1024px) {
+    p {
+      font-size: $h2-fs-desktop; 
+    }
 }
 </style>
 
