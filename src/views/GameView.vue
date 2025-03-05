6a189d4f69d2dba68ca6e16607bc92a920228c9d
NicoleSilfverling <92078589+NicoleSilfverling@users.noreply.github.com>
2025-03-05 14:22:37 +0100

    style: frog placement

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -24,46 +24,28 @@ function hideFrog() {
 
 <template>
     <div class="container">
-        <div class="frog" v-on:mouseover="foundFrog" v-on:mouseleave="hideFrog">
-            <img src="../assets/figma_components/logo.png" />
-        </div>
-
         <div class="message" v-if="frogIsFound">
             <h3>Grattis!</h3>
             <p>Du hittade grodan!</p>
+        </div>
+        <div class="frog" v-on:mouseover="foundFrog" v-on:mouseleave="hideFrog">
+            <img src="../assets/figma_components/logo.png" />
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.container{
+.container {
     width: 100vw;
     height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
     position: relative;
-    overflow: hidden; 
-    
-}
-.frog {
-    position: absolute;
-    top: calc(50% + 100px); 
-    right: 20%;
-    opacity: 0;
-    width: 64px;
-    height: 64px;
-    img {
-        width: 100%;
-        height: 100%;
-    }
-}
-.frog:hover {
-    opacity: 100;
 }
 
 .message {
+    position: absolute;
+    bottom: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     color: $text-color;
     background-color: rgba(0, 0, 0, 0.8);
     border-radius: 22.76px;
@@ -72,9 +54,25 @@ function hideFrog() {
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    align-items: center;
     width: 263px;
     height: 146.02px;
-    padding: 2px 0px 0.02px 0px;
+    padding: 10px;
+}
+
+.frog {
+    position: fixed;
+    bottom: 20%;
+    right: 10%;
+    opacity: 0;
+    width: 64px;
+    height: 64px;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+}
+.frog:hover {
+    opacity: 1;
 }
 </style>
