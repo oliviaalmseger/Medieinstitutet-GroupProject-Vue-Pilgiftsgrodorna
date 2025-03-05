9e3d1f445bc674df22ca5b63237fe4ec2326c521
Alice Swahn <aliceswahn@hotmail.com>
2025-03-05 09:26:28 +0100

    fix: remove import to contact view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
     import { createRouter, createWebHistory} from "vue-router";
     import StartPageView from "../views/StartPageView.vue";
-    import ContactView from "../views/ContactView.vue";
 
     const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL), 
