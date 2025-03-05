98959e282b39490b9a9b4d02784152f8fb83ef7d
Moa Magnfors <moa@magnfors.se>
2025-03-05 10:58:25 +0100

    feat: add InfoCard component
    - script
    - html

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    heading: String,
+    content: String,
+});
+</script>
+
+<template>
+    <section class="info-card">
+        <h3>{{ heading }}</h3>
+        <p>{{ content }}</p>
+    </section>
+</template>
