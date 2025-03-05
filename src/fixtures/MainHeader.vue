f2a9b7ee2291538d1c569c54b13a140469012d88
AgnesWilson <agnes.wilson97@hotmail.com>
2025-03-05 10:37:00 +0100

    Feat: added styling to nav-bar
    Co-Authored-By: Olivia <166169277+oliviaalmseger@users.noreply.github.com>

--- src/fixtures/MainHeader.vue
+++ src/fixtures/MainHeader.vue
@@ -1,16 +1,93 @@
 <script setup lang="js">
-import { RouterLink } from "vue-router";
+import { RouterLink } from "vue-router"
+
 </script>
 
 <template>
     <header>
     <nav>
-      <RouterLink to="/start">Hem</RouterLink>
-      <RouterLink to="/startGame">Spel</RouterLink>
-      <RouterLink to="/contactView">Kontakt</RouterLink>
+        <div class="nav-bg">
+            <RouterLink class="nav-btn" to="/start">Hem</RouterLink>
+        </div>
+
+        <div class="nav-bg">
+        <RouterLink class="nav-btn" to="/startGame">Spel</RouterLink>
+        </div>
+
+        <div class="nav-bg">
+         <RouterLink class="nav-btn" to="/contactView">Kontakt</RouterLink>
+        </div>
     </nav>
   </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+header {
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+
+
+    .nav-bg {
+        background-image: url(../assets/figma_components/nav-button.png);
+        background-size: cover; 
+        background-position: center;
+        width: 90px;
+        height: 30px;
+        alt: "white plank with rounded corners acting as a background to the navigation buttons";
+
+        display: inline-block;
+        margin-inline: 5px;
+    
+
+        .nav-btn {
+        padding-top: 5px;
+        display: flex;
+        justify-content: center;
+        padding-top: 9px;
+
+        
+        font-family: $link-font;
+        font-size: $link-fontsize;
+        color: $text-color;
+        text-decoration: none;
+        cursor: pointer;
+        }
+    }
+}
+
+
+
+
+// header {
+//     display: flex; 
+//     justify-content: center;
+//     align-items: center;
+
+//     .nav-bg {
+//         background-image: url(../assets/figma_components/nav-button.png);
+//         background-size: cover; 
+//         background-position: center;
+//         width: 90px;
+//         height: 30px;
+//         alt: "white plank with rounded corners acting as a background to the navigation buttons";
+        
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         flex-direction: row;
+
+//         .nav-btn {
+//         padding-top: 5px;
+//         display: flex;
+        
+//         font-family: $link-font;
+//         font-size: $link-fontsize;
+//         color: $text-color;
+//         text-decoration: none;
+//         cursor: pointer;
+//         }
+//     }
+// }
+
 </style>
