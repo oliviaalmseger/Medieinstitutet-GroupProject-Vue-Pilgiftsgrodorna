11d075b6017fe1d963c80e7eeaa6df26130cc3ae
AgnesWilson <agnes.wilson97@hotmail.com>
2025-03-06 13:07:13 +0100

    🚚 Move frog-stying
    moved the styling for the frog placement to separate scss files for tablet, desktop and mobile
    
    Co-Authored-By: Olivia <166169277+oliviaalmseger@users.noreply.github.com>

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -29,55 +29,9 @@ defineProps({
     p {
         margin-bottom: 2.5rem;
     }
-
-    .blue-frog {
-        width: 100px;
-        height: auto;
-
-        position: absolute;
-        left: 330px;
-        top: 600px;
-    }
-
-    .purple-frog {
-        transform: scaleX(-1); 
-        width: 100px;
-        height: auto;
-
-        position: absolute;
-        left: 355px;
-        top: 1117px;
-    }
-
-        .yellow-blue-frog {
-        transform: scaleX(-1);
-        width: 120px;
-        height: auto;
-
-        position: absolute;
-        left: 130px;
-        bottom: 978px;
-    }
-
-    .red-frog {
-        transform: scaleX(-1); 
-        width: 170px;
-        height: auto;
-
-        position: absolute;
-        left: 290px;
-        bottom: 365px;
-    }
-    
-    .yellow-frog {
-        width: 110px;
-        height: auto;
-
-        position: absolute;
-        left: 150px;
-        bottom: 325px;        
-    }
 }
+@import 'FrogStyles/FrogStylesMobile.scss';
+
 
 @media (min-width: 834px) {
     .info-card {
@@ -87,55 +41,8 @@ defineProps({
         p {
             margin-bottom: 1.5rem;
         }
-
-        .blue-frog {
-        width: 130px;
-        height: auto;
-
-        position: absolute;
-        left: 580px;
-        top: 780px;
-        }
-
-        .purple-frog {
-        transform: scaleX(-1); 
-        width: 130px;
-        height: auto;
-
-        position: absolute;
-        left: 600px;
-        top: 1240px;
-        }
-
-        .yellow-blue-frog {
-        transform: scaleX(-1);
-        width: 180px;
-        height: auto;
-
-        position: absolute;
-        left: 40px;
-        bottom: 810px;
-        }
-
-        .red-frog {
-        transform: scaleX(-1); 
-        width: 170px;
-        height: auto;
-
-        position: absolute;
-        left: 565px;
-        bottom: 350px;
-        }
-    
-        .yellow-frog {
-        width: 175px;
-        height: auto;
-
-        position: absolute;
-        left: 110px;
-        bottom: 255px;        
-        }
     }
+    @import 'FrogStyles/FrogStylesTablet.scss';
 }
 
 @media (min-width: 1280px) {
@@ -145,54 +52,7 @@ defineProps({
         padding: 1.5rem 1rem 0 1rem;
         margin-bottom: 20px;
         margin-right: 8px;
-
-        .blue-frog {
-        width: 150px;
-        height: auto;
-
-        position: absolute;
-        left: 250px;
-        top: 582px;
-        }
-
-        .purple-frog {
-        transform: scaleX(-1); 
-        width: 150px;
-        height: auto;
-
-        position: absolute;
-        left: 680px;
-        top: 790px;
-        }
-
-        .yellow-blue-frog {
-        transform: scaleX(-1);
-        width: 180px;
-        height: auto;
-
-        position: absolute;
-        left: 840px;
-        bottom: 700px;
-        }
-
-        .red-frog {
-        transform: scaleX(-1); 
-        width: 200px;
-        height: auto;
-
-        position: absolute;
-        left: 215px;
-        bottom: 70px;
-        }
-    
-        .yellow-frog {
-        width: 160px;
-        height: auto;
-
-        position: absolute;
-        left: 395px;
-        bottom: 335px;        
-        }
     }
+    @import 'FrogStyles/FrogStylesDesktop.scss';
 }
 </style>
