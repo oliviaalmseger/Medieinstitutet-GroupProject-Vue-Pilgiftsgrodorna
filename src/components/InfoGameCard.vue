1f765b546ea48cbf2a23e394256f99b358f36f1c
Moa Magnfors <moa@magnfors.se>
2025-03-06 12:07:13 +0100

    feat: add InfoGameCard component

---
+++ src/components/InfoGameCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        <div class="infogame-container">
+            <section class="info-game-card">
+                <h2>Hitta pilgiftsgrodan!</h2>
+                <p>
+                    Som du fått lära dig så tycker pilgiftsgrodan om att
+                    utforska. Någonstans i djungeln är det en som är på
+                    upptäcktsfärd. Kan du hitta den?
+                </p>
+                <p>Dra med musen över djungeln för att leta efter grodan!</p>
+                <img
+                    src="../assets/figma_components/frog-yellow-blue.png"
+                    class="blueyellow-frog"
+                />
+
+                <div class="start-game-btn">
+                    <button>Starta spelet</button>
+                </div>
+            </section>
+        </div>
+    </main>
+</template>
