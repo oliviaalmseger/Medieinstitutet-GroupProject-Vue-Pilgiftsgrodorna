0be6cb5b82c8903b6703ddd9bf53c8e04ae04a5b
Moa Magnfors <moa@magnfors.se>
2025-03-10 10:13:52 +0100

    :art: style: fix footer placement

--- src/fixtures/MainFooter.vue
+++ src/fixtures/MainFooter.vue
@@ -1,20 +1,32 @@
 <script setup lang="js"></script>
 
 <template>
-    <div class="footer">
-        <span>© Poison Meerkats x Pilgiftsgrodan 2025</span>
-    </div>
+    <footer>
+        <div class="footer">
+            <span>© Poison Meerkats x Pilgiftsgrodan 2025</span>
+        </div>
+    </footer>
 </template>
 
 <style scoped lang="scss">
-.footer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+#app {
+    position: relative;
 
-    background-color: $text-color;
-    height: 35px;
-    font-family: $h3-font;
-    font-size: 1rem;
+    footer {
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+
+        .footer {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            background-color: $text-color;
+            height: 35px;
+            font-family: $h3-font;
+            font-size: 1rem;
+        }
+    }
 }
 </style>
