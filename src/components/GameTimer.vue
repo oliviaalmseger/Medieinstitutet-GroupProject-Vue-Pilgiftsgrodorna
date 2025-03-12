de568d5cfd72399d534bf5949f83d366bfc2c4e6
Alice Swahn <aliceswahn@hotmail.com>
2025-03-12 11:05:00 +0100

    style: format code with Prettier

--- src/components/GameTimer.vue
+++ src/components/GameTimer.vue
@@ -55,58 +55,56 @@ onUnmounted(() => {
 
 // Formattera tiden i formatet mm:ss
 const formattedTime = computed(() => {
-  const minutes = Math.floor(timeElapsed.value / 60);
-  const seconds = timeElapsed.value % 60;
-  if (minutes === 1) {
-    return `${minutes} minut och ${seconds.toString()} sekunder`;
-  } 
-  else if (minutes > 1) {
-    return `${minutes} minuter och ${seconds.toString()} sekunder`;
-  } 
-  else {
-    return `${seconds.toString()} sekunder`;
-  }
+    const minutes = Math.floor(timeElapsed.value / 60);
+    const seconds = timeElapsed.value % 60;
+    if (minutes === 1) {
+        return `${minutes} minut och ${seconds.toString()} sekunder`;
+    } else if (minutes > 1) {
+        return `${minutes} minuter och ${seconds.toString()} sekunder`;
+    } else {
+        return `${seconds.toString()} sekunder`;
+    }
 });
 </script>
 
 <template>
-  <div class="timer-container">
-    <div class="timer">
-      <p>Spelad tid:</p>
-      <p>{{ formattedTime }}</p>
+    <div class="timer-container">
+        <div class="timer">
+            <p>Spelad tid:</p>
+            <p>{{ formattedTime }}</p>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped lang="scss">
 .timer-container {
-position: fixed;
-top: 0;
-left: 0;
-right: 0;
-display: flex;
-justify-content: center;
-padding-top: 50px;
-
-  .timer {
-    background: url('/src/assets/figma_components/wood-sign-big.avif')
-      no-repeat center / cover;
-    background-size: contain;
-    width: 400px;
-    height: 75px;
-
-    display: flex; 
-    flex-direction: column; 
-    justify-content: center; 
-    
-    p {
-    font-family: $button-font;
-    font-size: $button-fs;
-    color: #000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
     display: flex;
     justify-content: center;
-    text-align: center;
+    padding-top: 50px;
+
+    .timer {
+        background: url('/src/assets/figma_components/wood-sign-big.avif')
+            no-repeat center / cover;
+        background-size: contain;
+        width: 400px;
+        height: 75px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        p {
+            font-family: $button-font;
+            font-size: $button-fs;
+            color: #000;
+            display: flex;
+            justify-content: center;
+            text-align: center;
+        }
     }
-  }
 }
 </style>
