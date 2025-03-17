20849c5f4d0714253abae719aa7c436715d1f6c4
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-03-17 11:05:26 +0100

    fix: changed base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/fed24d-grafiska-verktyg-pilgiftsgrodorna/',
+    base: '/Medieinstitutet-GroupProject-Vue-Pilgiftsgrodorna/',
     plugins: [vue()],
     resolve: {
         alias: {
